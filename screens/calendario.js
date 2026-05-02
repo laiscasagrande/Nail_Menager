9bbf4638e278c24f7c69d75a5b7bc89a4ac524c1
Nathan <165735571+NathanRochaGomes@users.noreply.github.com>
2026-05-01 23:30:10 -0300

    Revert "Ajustes de versão."
    This reverts commit 5e78ab1e5d0203cc1111a66e803d840021efa7ea.

--- screens/calendario.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HojariosDisponiveis() {
-    return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Calendário será aqui</Text>
-        </View>
-    );
-}
