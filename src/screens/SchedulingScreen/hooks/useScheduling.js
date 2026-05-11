9819b75f1d37d892720a2e458fa14c4f9f286cc2
laiscasagrande <laisluanaka@outlook.com>
2026-05-11 10:19:37 -0300

    Refatoração de código. Movendo toda a lógica do componente para o hook useScheduling

---
+++ src/screens/SchedulingScreen/hooks/useScheduling.js
@@ -0,0 +1,163 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { formScheduling } from "../../../schemas/schedulingSchema";
+import { v4 as uuidv4 } from 'uuid';
+import { COLORS } from "../../../constants/colors";
+import { DraggingEvent } from "@howljs/calendar-kit";
+import { View } from "react-native";
+
+export function useScheduling() {
+
+    const [events, setEvents] = useState([]);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [idEvent, setIdEvent] = useState(null)
+
+    const bottomSheetRef = useRef(null);
+
+    const methods = useForm({
+        resolver: zodResolver(formScheduling),
+        defaultValues: {
+            event: "",
+            dateStart: new Date(),
+            dateEnd: new Date(),
+        },
+    })
+
+    const handleDragCreateStart = (start) => {
+        console.log("começou:", start);
+    };
+
+    const handleDragCreateEnd = (event) => {
+        if (!event) return;
+
+        bottomSheetRef.current.expand();
+
+        const createdEvent = {
+            ...event,
+            id: uuidv4(),
+            title: "",
+            color: COLORS.primary,
+        };
+
+        methods.reset({
+            event: "",
+            dateStart: new Date(event.start.dateTime),
+            dateEnd: new Date(event.end.dateTime),
+        });
+
+        setIdEvent(createdEvent.id)
+
+        setEvents((prev) => [...prev, createdEvent]);
+    };
+
+    const handleDragStart = (event) => {
+        console.log("editando:", event);
+    };
+
+    const handleDragEnd = (event) => {
+        console.log(event)
+        setEvents((prev) =>
+            prev.map((e) =>
+                e.id === event.id
+                    ? {
+                        ...e,
+                        ...event,
+                    }
+                    : e
+            )
+        );
+
+        setSelectedEvent(null);
+    };
+
+    function onSubmit(data) {
+        console.log(data)
+        setEvents((prev) =>
+            prev.map((e) =>
+                e.id === idEvent
+                    ? {
+                        ...e,
+                        title: data.event,
+                        start: {
+                            dateTime: data.dateStart.toISOString(),
+                            timeZone: "local",
+                        },
+                        end: {
+                            dateTime: data.dateEnd.toISOString(),
+                            timeZone: "local",
+                        },
+                    }
+                    : e
+            )
+        )
+        bottomSheetRef.current.close()
+        setIdEvent(null)
+        setSelectedEvent(null)
+        methods.reset()
+    }
+
+    function handlePressEvent(event) {
+        setSelectedEvent(event)
+
+        const start = new Date(event.start.dateTime)
+        const end = new Date(event.end.dateTime)
+
+        methods.reset({
+            event: event.title,
+            dateStart: start,
+            dateEnd: end
+        })
+
+        setIdEvent(event.id)
+
+        bottomSheetRef.current.expand()
+    }
+
+    const renderDraggingEvent = useCallback((props) => {
+        return (
+            <DraggingEvent
+                {...props}
+                TopEdgeComponent={
+                    <View
+                        style={{
+                            height: 10,
+                            width: "100%",
+                            backgroundColor: COLORS.primary,
+                            position: "absolute",
+                        }}
+                    />
+                }
+                BottomEdgeComponent={
+                    <View
+                        style={{
+                            height: 10,
+                            width: "100%",
+                            backgroundColor: COLORS.primary,
+                            bottom: 0,
+                            position: "absolute",
+                        }}
+                    />
+                }
+            />
+        );
+    }, []);
+
+    return {
+        events,
+        selectedEvent,
+        bottomSheetRef,
+        methods,
+
+        handlers: {
+            handleDragCreateStart,
+            handleDragCreateEnd,
+            handleDragStart,
+            handleDragEnd,
+            handlePressEvent,
+            onSubmit,
+        },
+
+        renderDraggingEvent,
+    }
+}
--- src/screens/SchedulingScreen/index.js
+++ src/screens/SchedulingScreen/index.js
@@ -1,152 +1,15 @@
-import React, { useCallback, useRef, useState } from "react";
 import {
     CalendarContainer,
     CalendarHeader,
-    CalendarBody,
-    DraggingEvent,
+    CalendarBody
 } from "@howljs/calendar-kit";
 import FormSheetScheduling from "./components/FormSheetScheduling";
-import { COLORS } from "../../constants/colors";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { FormProvider, useForm } from "react-hook-form";
-import { View } from "react-native";
-import { v4 as uuidv4 } from 'uuid';
-import { formScheduling } from "../../schemas/schedulingSchema";
+import { useScheduling } from "./hooks/useScheduling";
+import { FormProvider } from "react-hook-form";
 
 export default function SchedulingScreen() {
 
-    const [events, setEvents] = useState([]);
-    const [selectedEvent, setSelectedEvent] = useState(null);
-    const bottomSheetRef = useRef(null);
-    const [idEvent, setIdEvent] = useState(null)
-
-    const methods = useForm({
-        resolver: zodResolver(formScheduling),
-        defaultValues: {
-            event: "",
-            dateStart: new Date(),
-            dateEnd: new Date(),
-        },
-    })
-
-    const handleDragCreateStart = (start) => {
-        console.log("começou:", start);
-    };
-
-    const handleDragCreateEnd = (event) => {
-        if (!event) return;
-
-        bottomSheetRef.current.expand();
-
-        const createdEvent = {
-            ...event,
-            id: uuidv4(),
-            title: "",
-            color: COLORS.primary,
-        };
-
-        methods.reset({
-            event: "",
-            dateStart: new Date(event.start.dateTime),
-            dateEnd: new Date(event.end.dateTime),
-        });
-
-        setIdEvent(createdEvent.id)
-
-        setEvents((prev) => [...prev, createdEvent]);
-    };
-
-    const handleDragStart = (event) => {
-        console.log("editando:", event);
-    };
-
-    const handleDragEnd = (event) => {
-        console.log(event)
-        setEvents((prev) =>
-            prev.map((e) =>
-                e.id === event.id
-                    ? {
-                        ...e,
-                        ...event,
-                    }
-                    : e
-            )
-        );
-
-        setSelectedEvent(null);
-    };
-
-    function onSubmit(data) {
-        console.log(data)
-        setEvents((prev) =>
-            prev.map((e) =>
-                e.id === idEvent
-                    ? {
-                        ...e,
-                        title: data.event,
-                        start: {
-                            dateTime: data.dateStart.toISOString(),
-                            timeZone: "local",
-                        },
-                        end: {
-                            dateTime: data.dateEnd.toISOString(),
-                            timeZone: "local",
-                        },
-                    }
-                    : e
-            )
-        )
-        bottomSheetRef.current.close()
-        setIdEvent("")
-        setSelectedEvent(null)
-        methods.reset()
-    }
-
-    function handlePressEvent(event) {
-        setSelectedEvent(event)
-
-        const start = new Date(event.start.dateTime)
-        const end = new Date(event.end.dateTime)
-
-        methods.reset({
-            event: event.title,
-            dateStart: start,
-            dateEnd: end
-        })
-
-        setIdEvent(event.id)
-
-        bottomSheetRef.current.expand()
-    }
-
-    const renderDraggingEvent = useCallback((props) => {
-        return (
-            <DraggingEvent
-                {...props}
-                TopEdgeComponent={
-                    <View
-                        style={{
-                            height: 10,
-                            width: "100%",
-                            backgroundColor: COLORS.primary,
-                            position: "absolute",
-                        }}
-                    />
-                }
-                BottomEdgeComponent={
-                    <View
-                        style={{
-                            height: 10,
-                            width: "100%",
-                            backgroundColor: COLORS.primary,
-                            bottom: 0,
-                            position: "absolute",
-                        }}
-                    />
-                }
-            />
-        );
-    }, []);
+    const {events, selectedEvent, handlers, renderDraggingEvent, bottomSheetRef, methods} = useScheduling()
 
     return (
         <>
@@ -157,11 +20,11 @@ export default function SchedulingScreen() {
                 allowDragToEdit
                 dragStep={15}
                 selectedEvent={selectedEvent}
-                onPressEvent={(event) => handlePressEvent(event)}
-                onDragCreateEventStart={handleDragCreateStart}
-                onDragCreateEventEnd={handleDragCreateEnd}
-                onDragSelectedEventStart={handleDragStart}
-                onDragSelectedEventEnd={handleDragEnd}
+                onPressEvent={(event) => handlers.handlePressEvent(event)}
+                onDragCreateEventStart={handlers.handleDragCreateStart}
+                onDragCreateEventEnd={handlers.handleDragCreateEnd}
+                onDragSelectedEventStart={handlers.handleDragStart}
+                onDragSelectedEventEnd={handlers.handleDragEnd}
             >
                 <CalendarHeader />
 
@@ -172,7 +35,7 @@ export default function SchedulingScreen() {
                 <FormProvider {...methods}>
                     <FormSheetScheduling
                         bottomSheetRef={bottomSheetRef}
-                        onSubmit={onSubmit}
+                        onSubmit={handlers.onSubmit}
                     />
                 </FormProvider>
 
