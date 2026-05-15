d60422d976a2b87f2a4a8cc18f78ae6906555bc7
laiscasagrande <laisluanaka@outlook.com>
2026-05-15 14:29:13 -0300

    firebase adiciondo

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0iJ754KAb6CFhgPJGk-CVLU8QF9fBgzI",
+  authDomain: "nailmenager.firebaseapp.com",
+  projectId: "nailmenager",
+  storageBucket: "nailmenager.firebasestorage.app",
+  messagingSenderId: "161226514717",
+  appId: "1:161226514717:web:3806d5e5b312b0a1d681f4",
+};
+
+const fireBase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(fireBase);
